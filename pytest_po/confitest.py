adcc7c531c3a98bd57a355ce8d2491996ae97cb8
seredaa-ab <sereda.alina@chnu.edu.ua>
2026-04-29 11:09:56 +0300

    commit

---
+++ pytest_po/confitest.py
@@ -0,0 +1,10 @@
+import pytest
+from selenium import webdriver
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.get("https://www.greencity.cx.ua/#/greenCity/events")
+    driver.maximize_window()
+    yield driver
+    driver.quit()
